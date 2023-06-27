78022d365c27285f38baeb2dc04db4fe2b447500
jonathan_baraza <jbaraza46@gmail.com>
2023-06-27 21:00:23 +0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+// import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="text-red-500 text-3xl m-5 p-5">Hello world</div>
+    </>
+  );
+}
+
+export default App;
