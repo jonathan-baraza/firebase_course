2797d18eed6aa1b46617aeb5e18a2a8d2b225726
jonathan_baraza <jbaraza46@gmail.com>
2023-06-28 12:50:18 +0300

    inputs state

--- src/components/Auth.tsx
+++ src/components/Auth.tsx
@@ -1,6 +1,15 @@
-import React from "react";
+import { useState, useEffect } from "react";
+import { auth } from "../config/firebase";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { toast } from "react-toastify";
 
 export const Auth = () => {
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const handleSignIn = async () => {
+    toast("hello there");
+  };
+
   return (
     <div className="w-full flex justify-center min-h-[100vh] items-center p-3">
       <div className="border bg-[#f4ffff] flex flex-col space-y-4 items-center md:w-50 w-[90%] p-9 rounded-2xl shadow-sm">
@@ -8,16 +17,25 @@ export const Auth = () => {
 
         <input
           type="email"
-          placeholder="Enter Email"
+          name="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="Email"
           className="border p-2 rounded-xl w-full pl-6 outline-none"
         />
         <input
           type="password"
-          placeholder="Enter Password"
+          name="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="Password"
           className="border p-2 rounded-xl w-full pl-6 outline-none"
         />
         <div className="flex w-full justify-end">
-          <button className="bg-green-600 text-white p-2 mt-2  px-8 rounded-xl">
+          <button
+            onClick={handleSignIn}
+            className="bg-green-600 text-white p-2 mt-2  px-8 rounded-xl"
+          >
             Submit
           </button>
         </div>
