25452be3ebfc5cb49bbbc52461369ece40336e11
jonathan_baraza <jbaraza46@gmail.com>
2023-06-28 22:35:59 +0300

    import movie components

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import { Auth } from "./components/Auth";
 // import "./App.css";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import Movies from "./components/Movies";
 
 function App() {
   return (
     <>
       <div className="">
-        <Auth />
+        {/* <Auth /> */}
+        <Movies />
       </div>
       <ToastContainer pauseOnHover={false} autoClose={2000} />
     </>
