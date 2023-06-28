62e77acab6d71721cabe90a908918f6d6d11773e
jonathan_baraza <jbaraza46@gmail.com>
2023-06-28 12:29:45 +0300

    firebase auth setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import { useState } from "react";
+import { Auth } from "./components/Auth";
 // import "./App.css";
 
 function App() {
   return (
     <>
-      <div className="text-red-500 text-3xl m-5 p-5">Hello world</div>
+      <div className="">
+        <Auth />
+      </div>
     </>
   );
 }
--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,6 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-
+import { getAuth } from "firebase/auth";
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
@@ -12,6 +12,7 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
 
 //optional for this tutorial
 const analytics = getAnalytics(app);
