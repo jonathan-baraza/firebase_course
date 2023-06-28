7b58ad337c96427dc9a573e06c6d5a69e447f02b
jonathan_baraza <jbaraza46@gmail.com>
2023-06-28 22:40:51 +0300

    fn to get movies data

--- src/components/Movies.tsx
+++ src/components/Movies.tsx
@@ -1,5 +1,7 @@
 import { useState, useEffect } from "react";
 import { db } from "../config/firebase";
+import { toast } from "react-toastify";
+import { getDocs, collection } from "firebase/firestore";
 
 type movieType = {
   title: string;
@@ -9,10 +11,23 @@ type movieType = {
 
 const Movies = () => {
   const [movieList, setMovieList] = useState<movieType[]>([]);
+  const moviesCollectionRef = collection(db, "movies");
 
   const getMovieList = async () => {
-    //
+    try {
+      //READ THE DATABASE
+      const data = await getDocs(moviesCollectionRef);
+      console.log("data");
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+      if (error instanceof Error) toast.error(error.message);
+    }
   };
+
+  useEffect(() => {
+    getMovieList();
+  }, []);
   return <div>Movies </div>;
 };
 
