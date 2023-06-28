b3b0021fbff4432b365082a831a6bd3770928f9d
jonathan_baraza <jbaraza46@gmail.com>
2023-06-28 12:50:06 +0300

    auth import

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import { Auth } from "./components/Auth";
 // import "./App.css";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   return (
@@ -8,6 +10,7 @@ function App() {
       <div className="">
         <Auth />
       </div>
+      <ToastContainer />
     </>
   );
 }
