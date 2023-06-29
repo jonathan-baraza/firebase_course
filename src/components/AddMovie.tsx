fa00a6e3601ddff10c83586f01456123e14f5683
jonathan_baraza <jbaraza46@gmail.com>
2023-06-29 20:05:38 +0300

    component layout

--- src/components/AddMovie.tsx
+++ src/components/AddMovie.tsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 function AddMovie() {
-  return <div>AddMovie</div>;
+  return (
+    <div
+      className="fixed inset-0 flex items-center justify-center"
+      style={{ backgroundColor: "rgba(0,0,0,0.5)" }}
+    >
+      <div className="bg-white">Add New Movie</div>
+    </div>
+  );
 }
 
 export default AddMovie;
