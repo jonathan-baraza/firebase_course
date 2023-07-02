44a668f2345928db56c6eaa99fcb06fe410b3025
jonathan_baraza <jbaraza46@gmail.com>
2023-07-02 10:05:30 +0300

    firebase storage init

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -2,6 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
@@ -17,6 +18,7 @@ export const auth = getAuth(app);
 export const googleProvider = new GoogleAuthProvider();
 
 export const db = getFirestore(app);
+export const storage = getStorage(app);
 
 //optional for this tutorial
 const analytics = getAnalytics(app);
